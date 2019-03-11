e2be064f6de8deb9feb8bde8b68907ea85f665ac
git <sygnowski.m@gmail.com>
2019-03-11 11:18:45 +0000

    removed some part from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
-const path = require("path");
-
 module.exports = {
   entry: "./src/index.js",
   devtool: "source-map",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build")
+    filename: "bundle.js"
   },
   module: {
     rules: [
