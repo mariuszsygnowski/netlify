a907234b8267568b7fb97f4be8f72f3ce81c49c1
git <sygnowski.m@gmail.com>
2019-03-11 10:57:55 +0000

    revert to basic depedencies

--- webpack.config.js
+++ webpack.config.js
@@ -13,58 +13,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /(node_modules)/,
         loader: require.resolve("babel-loader")
-      },
-
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          {
-            loader: "css-loader"
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "less-loader" // compiles Less to CSS
-          }
-        ]
-      },
-
-      {
-        test: /\.(woff|woff2|eot|ttf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "fonts/[sha512:hash:base64:7].[ext]"
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "images/[sha512:hash:base64:7].[ext]"
-            }
-          }
-        ]
       }
     ]
   }
